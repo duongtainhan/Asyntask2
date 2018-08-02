f989070ec340c2b756c2dc32376e5a2e9ade70cd
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-08-02 18:44:31 +0700

    Init

--- app/src/main/java/com/example/duongtainhan555/asyntask/MainActivity.java
+++ app/src/main/java/com/example/duongtainhan555/asyntask/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.duongtainhan555.asyntask;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnPerform;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //anh xa
+        Init();
+        //EventClick
+        btnPerform.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+    private void Init()
+    {
+        btnPerform = findViewById(R.id.btnPerform);
+    }
+
 }
